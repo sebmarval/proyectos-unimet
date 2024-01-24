33574708fb60212dc7bded2fed91d94f86eae772
sebmarval <156669132+sebmarval@users.noreply.github.com>
2024-01-23 21:40:23 -0400

    Pelea Pokemon con arrays-12.py

--- prueba de oponentes-12.py
+++ prueba de oponentes-12.py
@@ -8,16 +8,19 @@ defensa_jugador = 100
 ataques = ["malicioso","placaje","ascuas"]
 
 while Ps_jugador > 0 and Ps_oponente > 0:
-    ataque_jugador = input("Elige un ataque de entre los tres posibles (malicioso, placaje y ascuas): ") 
-    if ataque_jugador == "malicioso":
+    ataque_jugador = input("ataque: ")
+    ataque_jugador = ataque_jugador.lower() 
+    if ataque_jugador == ataques(0):
+        print("has reducido la defensa de tu oponente en 10 puntos")
         defensa_oponente = defensa_oponente - 10
         if defensa_oponente <= 0:
-            print("has realizado 10 puntos de daño a tu enemigo")
-    elif ataque_jugador == "placaje":
-        print("la defensa de tu oponente se ha disminuido")
-        Ps_oponente -= 35 / (defensa_oponente/100)
-    elif ataque_jugador =="ascuas":
-        print("has realizado daño de ascuas")
+            defensa_oponente = 1
+    elif ataque_jugador == ataques(1):
+        print("has reducido la salud de tu oponente")
+        Ps_oponente -= 35 * (100/defensa_oponente)
+    elif ataque_jugador == ataques(2):
+        print("diste un golpe severo a tu oponente")
+        pass
         Ps_oponente -= 20 
     else:
         print("Que estás haciendo?! tus ataques son malicioso, placaje y ascuas!")
@@ -25,17 +28,17 @@ while Ps_jugador > 0 and Ps_oponente > 0:
     
     #Turno del oponente 
     #el turno del oponente se define con un random
-    ataque_oponente = random.randrange(1,3)
+    ataque_oponente = random.randrange(1,3+1)
     if ataque_oponente == 1: #látigo
-        print("tu oponente te ha hecho 10 de daño")
-        defensa_jugador = defensa_jugador - 10
-        if defensa_oponente <= 0:
-            defensa_oponente = 1
+        print("has recibido 10 puntos de daño!")
+        defensa_jugador -= 10
+        if defensa_jugador <= 0:
+            defensa_jugador = 1
     elif ataque_oponente == 2: #placaje
-        Ps_jugador -= 35 / (defensa_jugador/100)
-        print("te han atacado")
+        print("han reducido tu salud!")
+        Ps_jugador -= 35 * (100/defensa_jugador)
     elif ataque_oponente == 3: #pistola de agua
-        print("has recibido un fuerte ataque")
+        print("recibiste un golpe severo!")
         Ps_jugador -= 40
     #randrange está garantizado a ser 1,2 ó 3 
         
@@ -46,6 +49,9 @@ elif Ps_oponente <= 0: #ya sabemos que el jugador es >0
     print("has ganado!") 
 else: #ya sabemos que el oponente es > 0
     print("has perdido!")
-   
+
+    
+
+  
 
     
